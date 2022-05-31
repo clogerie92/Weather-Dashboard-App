ca350adf70793de666b4df3cc5034d9a9c51591f
clogerie <66565287+clogerie@users.noreply.github.com>
2022-05-31 15:44:55 -0400

    created global variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
     var searchHistory = JSON.parse(window.localStorage.getItem("search-history")) || [];
-    var searchBtn = $("#searchBtn")
+    var searchBtn = $(".searchBtn");
+    var deleteBtn = $(".deleteBtn");
+    var userInput = $("#user-input");
+    var historyList = $(".history-list");
+    var currentWeatherDiv = $("#current-weather");
+    var forecastDiv = $("#forecast");
 });
